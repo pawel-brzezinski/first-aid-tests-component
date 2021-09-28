1ea72fe6776193961a90f19c1809d135fb8c8995
Paweł Brzeziński <pawel.brzezinski@smartint.pl>
2021-09-28 16:05:10 +0200

    Add Symfony controller test case

--- tests/TestCase/Framework/Symfony/Controller/SymfonyControllerTestCase.php
+++ tests/TestCase/Framework/Symfony/Controller/SymfonyControllerTestCase.php
@@ -6,10 +6,11 @@ namespace PB\Component\FirstAidTests\Tests\TestCase\Framework\Symfony\Controller
 
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
-use Prophecy\Prophecy\{ObjectProphecy};
+use Prophecy\Prophecy\{MethodProphecy, ObjectProphecy};
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Twig\Environment;
+use Twig\Error\{LoaderError, RuntimeError, SyntaxError};
 
 /**
  * @author Paweł Brzeziński <pawel.brzezinski@smartint.pl>
@@ -60,5 +61,48 @@ abstract class SymfonyControllerTestCase extends TestCase
         $controller->setContainer($this->containerMock->reveal());
 
         return $controller;
+    }
+
+    /**
+     * @param string $id
+     * @param bool $expected
+     * @param int $calls
+     */
+    protected function mockHasService(string $id, bool $expected, int $calls = 1): void
+    {
+        /** @var MethodProphecy $methodProp */
+        $methodProp = $this->containerMock->has($id);
+        $methodProp->shouldBeCalledTimes($calls)->willReturn($expected);
+    }
+
+    /**
+     * @param string $id
+     * @param object $expected
+     * @param int $calls
+     */
+    protected function mockGetService(string $id, object $expected, int $calls = 1): void
+    {
+        /** @var MethodProphecy $methodProp */
+        $methodProp = $this->containerMock->get($id);
+        $methodProp->shouldBeCalledTimes($calls)->willReturn($expected);
+    }
+
+    /**
+     * @param array{string, array} $args            AbstractContainer::renderView() arguments
+     * @param string $expectedContent
+     * @param int $calls
+     *
+     * @throws LoaderError|RuntimeError|SyntaxError
+     */
+    protected function mockRenderView(array $args, string $expectedContent, int $calls = 1): void
+    {
+        list('view' => $view, 'parameters' => $parameters) = $args;
+
+        /** @var MethodProphecy $methodProp */
+        $methodProp = $this->twigMock->render($view, $parameters);
+        $methodProp->shouldBeCalledTimes($calls)->willReturn($expectedContent);
+
+        $this->mockHasService('twig', true, $calls);
+        $this->mockGetService('twig', $this->twigMock->reveal(), $calls);
     }
 }
